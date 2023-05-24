e800a1bd03018b15fdb6186c1ab373e7ad6772a7
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-24 12:50:08 +0100

    comment code

--- contracts/User.sol
+++ contracts/User.sol
@@ -1,65 +1,75 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
-contract User{
-    struct UserData
-     {
-        address userAddress;
-        string username;
-        uint256 balance
-        bool isLoggedIn
+contract User {
+    struct UserData {
+        bool isLoggedIn;
     }
 
-    mapping (address => UserData) private users;
+    // Mapping to store user data based on their address
+    mapping(address => UserData) private users;
 
-    event UserRegistered(address indexed userAddress, string userName);
+    // Event emitted when a new user is registered
+    event UserRegistered(address indexed userAddress);
+
+    // Event emitted when a user logs in
     event UserLoggedIn(address indexed userAddress);
+
+    // Event emitted when a user logs out
     event UserLoggedOut(address indexed userAddress);
-    event BalanceUpadated(address indexed userAddress, uint256 newBalance  );
-  
 
-function registerUser(string memory _username) public {
-    // check if user is  already registered
-    require(users[msg.sender].userAddress == address(0), "User already registered" );
+    /**
+     * @dev Registers a new user by setting their isLoggedIn status to false.
+     * Emits a UserRegistered event.
+     */
+    function registerUser() public {
+        // Check if the user is already registered
+        require(!users[msg.sender].isLoggedIn, "User already registered");
 
-    users[msg.sender] = UserData(msg.sender,  _username,0);
+        // Set the isLoggedIn status to false for the new user
+        users[msg.sender].isLoggedIn = false;
 
-    emit UserRegistered(msg.sender, _username)
-    
-}
-
-  function getUser(address _userAddress) public view returns (UserData memory) {
-        return users[_userAddress];
+        // Emit the UserRegistered event
+        emit UserRegistered(msg.sender);
     }
 
-function login()  public   {
-    require(!users[msg.sender].isLoggedIn, "User already logged in" );
-    users[msg.sender].isLoggedIn = true;
-    emit UserLoggedIn(msg.sender)
-}
+    /**
+     * @dev Logs in a user by setting their isLoggedIn status to true.
+     * Emits a UserLoggedIn event.
+     */
+    function login() public {
+        // Check if the user is already logged in
+        require(!users[msg.sender].isLoggedIn, "User already logged in");
 
-function logout() public  {
-    require(users[msg.sender].isLoggedIn, "User already logged out");
-    users[msg.sender].isLoggedIn = false;
-    emit UserLoggedOut(msg.sender)
-}
+        // Set the isLoggedIn status to true for the user
+        users[msg.sender].isLoggedIn = true;
 
- function isLoggedIn(address _userAddress) public view returns (bool) {
+        // Emit the UserLoggedIn event
+        emit UserLoggedIn(msg.sender);
+    }
+
+    /**
+     * @dev Logs out a user by setting their isLoggedIn status to false.
+     * Emits a UserLoggedOut event.
+     */
+    function logout() public {
+        // Check if the user is already logged out
+        require(users[msg.sender].isLoggedIn, "User already logged out");
+
+        // Set the isLoggedIn status to false for the user
+        users[msg.sender].isLoggedIn = false;
+
+        // Emit the UserLoggedOut event
+        emit UserLoggedOut(msg.sender);
+    }
+
+    /**
+     * @dev Retrieves the isLoggedIn status of a user.
+     * @param _userAddress The address of the user.
+     * @return A boolean indicating whether the user is logged in.
+     */
+    function isLoggedIn(address _userAddress) public view returns (bool) {
+        // Return the isLoggedIn status of the user
         return users[_userAddress].isLoggedIn;
     }
-
-    // set new  balance to  amount of  matic in  the user's wallet
-
-    function updateBalance(_newBalance) public{
-       users[msg.sender].balance = _newBalance
-
-       emit  BalanceUpadated(msg.sender, _newBalance);
-        
-    }
-   function getBalance(address _userAddress) public view returns (uint256) {
-        return users[_userAddress].balance;
-    }
-
-
 }
