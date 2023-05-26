0eb29e198fb49243b4681e24e4d90fcca2e1baad
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-26 13:22:11 +0100

    run test for the new implementations

--- test/User.js
+++ test/User.js
@@ -15,7 +15,7 @@ describe('User Contract', function () {
 	});
 
 	it('should register a user', async function () {
-		await userContract.registerUser('Alice', deployer.address);
+		await userContract.registerUser('Alice');
 		const alice = await userContract.getUser(deployer.address);
 		expect(alice.username).to.equal('Alice');
 	});
@@ -35,5 +35,25 @@ describe('User Contract', function () {
 		await userContract.logout();
 		const loggedOut = await userContract.isLoggedIn(deployer.address);
 		expect(loggedOut).to.be.false;
+	});
+
+	it('should update user points and retrieve leaderboard position', async function () {
+		await userContract.updatePoints(50);
+		const points = await userContract.getUserPoints(deployer.address);
+		expect(points).to.equal(50);
+
+		const leaderboardPosition = await userContract.getUserLeaderboardPosition(
+			deployer.address,
+		);
+		expect(leaderboardPosition).to.equal(1);
+	});
+
+	it('should retrieve the leaderboard', async function () {
+		const [addresses, points] = await userContract.getLeaderBoard();
+
+		expect(addresses).to.have.lengthOf(1);
+		expect(points).to.have.lengthOf(1);
+		expect(addresses[0]).to.equal(deployer.address);
+		expect(points[0]).to.equal(50);
 	});
 });
