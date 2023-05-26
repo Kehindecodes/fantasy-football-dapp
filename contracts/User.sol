6d4ba0ba952f8a264e8b409c8112528cb88b51e4
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-26 11:36:24 +0100

    add user  point and leaderboard functionality

--- contracts/User.sol
+++ contracts/User.sol
@@ -1,45 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @title User Contract
+ * @dev A contract to manage user registration, login/logout, balance, points, and leaderboard functionality.
+ */
 contract User {
     struct UserData {
         address userAddress;
         string username;
         uint256 balance;
         bool isLoggedIn;
+        uint256 totalPoints;
     }
+
+    // uint256 private totalUsers;
+    address[] private userAddresses;
 
     // Mapping to store user data based on their address
     mapping(address => UserData) private users;
 
-    // Event emitted when a new user is registered
+    /**
+     * @dev Event emitted when a new user is registered.
+     * @param userAddress The address of the registered user.
+     * @param username The username of the registered user.
+     */
     event UserRegistered(address indexed userAddress, string username);
 
-    // Event emitted when a user logs in
+    /**
+     * @dev Event emitted when a user logs in.
+     * @param userAddress The address of the logged-in user.
+     */
     event UserLoggedIn(address indexed userAddress);
 
-    // Event emitted when a user logs out
+    /**
+     * @dev Event emitted when a user logs out.
+     * @param userAddress The address of the logged-out user.
+     */
     event UserLoggedOut(address indexed userAddress);
 
+    /**
+     * @dev Event emitted when a user's balance is updated.
+     * @param userAddress The address of the user.
+     * @param newBalance The updated balance of the user.
+     */
     event BalanceUpdated(address indexed userAddress, uint256 newBalance);
+
+    /**
+     * @dev Event emitted when a user's total points are updated.
+     * @param userAddress The address of the user.
+     * @param totalPoints The updated total points of the user.
+     */
+    event UserPointsUpdated(address indexed userAddress, uint256 totalPoints);
 
     /**
      * @dev Registers a new user by setting their isLoggedIn status to false.
      * Emits a UserRegistered event.
+     * @param _username The username of the new user.
      */
-    function registerUser(
-        string memory _username,
-        address _userAddress
-    ) public {
+    function registerUser(string memory _username) public {
         require(
-            users[_userAddress].userAddress == address(0),
+            users[msg.sender].userAddress == address(0),
             "User already registered"
         );
-        users[_userAddress] = UserData(_userAddress, _username, 0, false);
+        users[msg.sender] = UserData(msg.sender, _username, 0, false, 0);
+        userAddresses.push(msg.sender); // Add the user address to the array
 
-        emit UserRegistered(_userAddress, _username);
+        emit UserRegistered(msg.sender, _username);
     }
 
+    /**
+     * @dev Retrieves the user data for a given user address.
+     * @param _userAddress The address of the user.
+     * @return The user data (userAddress, username, balance, isLoggedIn, totalPoints).
+     */
     function getUser(
         address _userAddress
     ) public view returns (UserData memory) {
@@ -51,12 +85,9 @@ contract User {
      * Emits a UserLoggedIn event.
      */
     function login() public {
-        // Check if the user is already logged in
         require(!users[msg.sender].isLoggedIn, "User already logged in");
-        // Set the isLoggedIn status to true for the user
         users[msg.sender].isLoggedIn = true;
 
-        // Emit the UserLoggedIn event
         emit UserLoggedIn(msg.sender);
     }
 
@@ -65,13 +96,9 @@ contract User {
      * Emits a UserLoggedOut event.
      */
     function logout() public {
-        // Check if the user is already logged out
         require(users[msg.sender].isLoggedIn, "User already logged out");
-
-        // Set the isLoggedIn status to false for the user
         users[msg.sender].isLoggedIn = false;
 
-        // Emit the UserLoggedOut event
         emit UserLoggedOut(msg.sender);
     }
 
@@ -81,16 +108,114 @@ contract User {
      * @return A boolean indicating whether the user is logged in.
      */
     function isLoggedIn(address _userAddress) public view returns (bool) {
-        // Return the isLoggedIn status of the user
         return users[_userAddress].isLoggedIn;
     }
 
+    /**
+     * @dev Updates the balance of a user.
+     * Emits a BalanceUpdated event.
+     * @param _newBalance The new balance of the user.
+     */
     function updateBalance(uint256 _newBalance) public {
         users[msg.sender].balance = _newBalance;
+
         emit BalanceUpdated(msg.sender, _newBalance);
     }
 
+    /**
+     * @dev Retrieves the balance of a user.
+     * @param _userAddress The address of the user.
+     * @return The balance of the user.
+     */
     function getBalance(address _userAddress) public view returns (uint256) {
         return users[_userAddress].balance;
+    }
+
+    /**
+     * @dev Updates the total points of a user.
+     * Emits a UserPointsUpdated event.
+     * @param _points The points to be added to the user's total points.
+     */
+    function updatePoints(uint256 _points) public {
+        UserData storage user = users[msg.sender];
+        user.totalPoints += _points;
+
+        emit UserPointsUpdated(msg.sender, user.totalPoints);
+    }
+
+    /**
+     * @dev Retrieves the total points of a user.
+     * @param _userAddress The address of the user.
+     * @return The total points of the user.
+     */
+    function getUserPoints(address _userAddress) public view returns (uint256) {
+        return users[_userAddress].totalPoints;
+    }
+
+    /**
+     * @dev Retrieves the leaderboard, sorted in descending order based on total points.
+     * @return An array of user addresses and an array of their corresponding total points.
+     */
+    function getLeaderboard()
+        public
+        view
+        returns (address[] memory, uint256[] memory)
+    {
+        uint256 totalUsers = getTotalUsers();
+        address[] memory addresses = new address[](totalUsers);
+        uint256[] memory points = new uint256[](totalUsers);
+
+        for (uint256 i = 0; i < totalUsers; i++) {
+            addresses[i] = getUserAddressAtIndex(i);
+            points[i] = users[addresses[i]].totalPoints;
+        }
+
+        sortLeaderboard(addresses, points);
+        return (addresses, points);
+    }
+
+    /**
+     * @dev Retrieves the total number of registered users.
+     * @return The total number of registered users.
+     */
+    function getTotalUsers() private view returns (uint256) {
+        return userAddresses.length;
+    }
+
+    /**
+     * @dev Retrieves the user address at the specified index in the userAddresses array.
+     * @param index The index of the user address.
+     * @return The user address at the specified index.
+     */
+    function getUserAddressAtIndex(
+        uint256 index
+    ) private view returns (address) {
+        require(index < userAddresses.length, "Invalid index");
+        return userAddresses[index];
+    }
+
+    /**
+     * @dev Sorts the leaderboard in descending order based on total points using bubble sort algorithm.
+     * @param addresses The array of user addresses.
+     * @param points The array of total points.
+     */
+    function sortLeaderboard(
+        address[] memory addresses,
+        uint256[] memory points
+    ) private pure {
+        uint256 n = points.length;
+        for (uint256 i = 0; i < n - 1; i++) {
+            for (uint256 j = 0; j < n - i - 1; j++) {
+                if (points[j] < points[j + 1]) {
+                    // Swap addresses
+                    (addresses[j], addresses[j + 1]) = (
+                        addresses[j + 1],
+                        addresses[j]
+                    );
+                    // Swap points
+                    (points[j], points[j + 1]) = (points[j + 1], points[j]);
+                }
+            }
+        }
     }
 }
