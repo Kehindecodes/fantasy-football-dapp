5f325fd3979d14a7e96207c2f710ab5e334d039a
Oki Kehinde <olamiposi.oki@gmail.com>
2023-06-06 12:11:41 +0100

    comment  contract code

--- contracts/PlayerContract.sol
+++ contracts/PlayerContract.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+// This contract defines the PlayerContract, which manages player information and game week points.
+
 contract PlayerContract {
+    // Enum to represent player positions
     enum PlayerPosition {
         Striker,
         Midfielder,
@@ -9,6 +12,7 @@ contract PlayerContract {
         Goalkeeper
     }
 
+    // Struct to represent a player
     struct Player {
         uint256 playerId;
         string playerName;
@@ -24,9 +28,13 @@ contract PlayerContract {
         uint256 overallPoints;
     }
 
+    // Array to store all players
     Player[] public players;
+
+    // Mapping to store game week points for each player
     mapping(uint256 => mapping(uint256 => uint256)) public gameWeekPoints;
 
+    // Event emitted when a new player is added
     event PlayerAdded(
         uint256 playerId,
         string playerName,
@@ -34,8 +42,15 @@ contract PlayerContract {
         PlayerPosition position
     );
 
+    // Event emitted when game week points are set for a player
     event GameWeekPointsSet(uint256 playerId, uint256 gameWeek, uint256 points);
 
+    /**
+     * @dev Adds a new player to the contract.
+     * @param _playerName The name of the player.
+     * @param _playerPrice The price of the player.
+     * @param _position The position of the player.
+     */
     function addPlayer(
         string memory _playerName,
         uint256 _playerPrice,
@@ -61,6 +76,12 @@ contract PlayerContract {
         emit PlayerAdded(playerId, _playerName, _playerPrice, _position);
     }
 
+    /**
+     * @dev Sets the game week points for a player.
+     * @param _playerId The ID of the player.
+     * @param _gameWeek The game week number.
+     * @param _points The points earned by the player in the game week.
+     */
     function setGameWeekPoints(
         uint256 _playerId,
         uint256 _gameWeek,
@@ -72,6 +93,12 @@ contract PlayerContract {
         emit GameWeekPointsSet(_playerId, _gameWeek, _points);
     }
 
+    /**
+     * @dev Gets the game week points for a player.
+     * @param _playerId The ID of the player.
+     * @param _gameWeek The game week number.
+     * @return The points earned by the player in the game week.
+     */
     function getGameWeekPoints(
         uint256 _playerId,
         uint256 _gameWeek
@@ -81,10 +108,20 @@ contract PlayerContract {
         return gameWeekPoints[_playerId][_gameWeek];
     }
 
+    /**
+     * @dev Gets the price of a player.
+     * @param _playerId The ID of the player.
+     * @return The price of the player.
+     */
     function getPlayerPrice(uint256 _playerId) public view returns (uint256) {
         return players[_playerId].playerPrice;
     }
 
+    /**
+     * @dev Gets the position of a player.
+     * @param _playerId The ID of the player.
+     * @return The position of the player.
+     */
     function getPlayerPosition(
         uint256 _playerId
     ) public view returns (PlayerPosition) {
